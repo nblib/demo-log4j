74e541bcce7784ec6969a9792ceff3ce016f0dde
heweifeng <heweifeng@autohome.com.cn>
2017-10-16 14:32:20 +0800

    log4j2的简单实用

--- src/main/java/demo/hewe/App.java
+++ src/main/java/demo/hewe/App.java
@@ -1,11 +1,19 @@
 package demo.hewe;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    static Logger log = LoggerFactory.getLogger(App.class);
 
+    public static void main(String[] args) {
+        log.error("nihaoya");
+        log.info("nihaoya");
+        log.warn("nihaoya");
+        log.trace("nihaoya");
+        log.debug("nihaoya");
     }
 }
