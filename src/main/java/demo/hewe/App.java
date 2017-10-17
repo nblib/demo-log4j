c9f708769b1d9e8b5f21beafe89ed898ea8be770
heweifeng <heweifeng@autohome.com.cn>
2017-10-17 15:52:12 +0800

    log4j的简单实用

--- src/main/java/demo/hewe/App.java
+++ src/main/java/demo/hewe/App.java
@@ -1,11 +1,21 @@
 package demo.hewe;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    static Logger log = Logger.getLogger(App.class);
 
+    public static void main(String[] args) {
+       test();
+
+    }
+    public static void test(){
+        log.warn("字符串为空");
+        log.error("数字转化错误");
+        log.info("计算完成");
+        log.debug("开始计算");
     }
 }
